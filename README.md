243fe272f2de54faa0a95381f4e685d6ca16a33b
DanielaCalisaya <hdaniela127c@gmail.com>
2022-02-15 14:56:36 -0600

    index.js and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Manipulando elementos con JavaScript
+
+## Clase 92, 94
+
+## Ubicacion -> Programación/TRABAJOS PRACTICOS/Manipulando elementos con JavaScript
--- public/js/index.js
+++ public/js/index.js
@@ -23,11 +23,37 @@ function qs (element) {
 let h2 = qs('.subtitulo')
 let a = qs('a') /* al hacer esta funcion hacemos menos codigo */
 let p = document.querySelector('p')
+let $body = qs('body');
+
+let userName = prompt('Ingrese su nombre') || 'Invitad@';
+userName.trim() !== "" ? $h2.innerText += `${userName}` : $h2.innerText += 'Invitado';
+main.style.display = "block"
+$h2.style.textTransform = 'uppercase';
+$a.style.color = '#E51B3E';
+/* clases */
+let background = confirm('¿Desea colocar un fondo de pantalla?');
+background && $body.classList.add('fondo'); element.classList.remove.toggle('fondo')
+
+/* for(let i=0; i < $p.length; i++){
+    if(i % 2 == 0){
+        $p[i].classList.add('destacadoPar');
+    }else{
+        $p[i].classList.add('destacadoImpar');
+    }
+} */
+
+$p.forEach((element) => {
+    if(index % 2 == 0){
+        element.classList.add('destacadoPar');
+    }else{
+        element.classList.add('destacadoImpar')
+    }
+})
 
 console.log(main)
 console.log(h2)
 console.log(a)
 console.log(p)
 
-main.style.display = "block"
+/* completar los siguientes desafios */
 
